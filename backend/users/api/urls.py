8473f0ea85264da38e86c0c4fa61a63669952318
arsham <aqaarsham@gmail.com>
2022-07-17 19:46:07 +0430

    Fix CRUD method for each endpoint correctly

--- backend/users/api/urls.py
+++ backend/users/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from dj_rest_auth.registration.views import RegisterView, LoginView
 from dj_rest_auth.views import LogoutView, PasswordChangeView
-from .views import UserDetailApiView, RevokeApiKeyView
+from .views import (
+    UserDetailApiView, RevokeApiKeyView,
+    UserUpdateApiView, UserDeleteApiView
+)
 
 app_name = 'api'
 
@@ -11,7 +14,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('password_change/', PasswordChangeView.as_view(), name='password_change'),
     path('user/', UserDetailApiView.as_view(), name='user_detail'),
-    path('user/update/', UserDetailApiView.as_view(), name='user_update'),
-    path('user/delete/', UserDetailApiView.as_view(), name='user_delete'),
+    path('user/update/', UserUpdateApiView.as_view(), name='user_update'),
+    path('user/delete/', UserDeleteApiView.as_view(), name='user_delete'),
     path('revoke_key/', RevokeApiKeyView.as_view(), name='revoke_key'),
 ]
