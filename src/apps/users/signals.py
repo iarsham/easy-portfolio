be4c5d55aa01b3642d17fb22a6224001955e915b
iarsham <aqaarsham@gmail.com>
2022-09-03 04:41:26 +0430

    Change user Signal for create an aboutme and education object

--- src/apps/users/signals.py
+++ src/apps/users/signals.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from apps.portfolio.models import AboutMe, Education
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_token(sender, instance, created, **kwargs):
     if created:
         Token.objects.create(user=instance)
+        obj = AboutMe.objects.create(user=instance)
+        Education.objects.create(about_me=obj)
